f06b336c761ec5862146afa9d28c8d0a73b51c64
Christoffer Wallman <christoffer.wallman@studerande.karlskogafhs.se>
2023-03-25 21:20:37 +0100

    add collapsable menu and links

---
+++ src/js/smoothScroll.js
@@ -0,0 +1,17 @@
+function scrolltoProjects(){
+    var access = document.getElementById("Projects");
+    access.scrollIntoView({behavior: 'smooth'}, true);
+}
+function scrolltoAboutMe(){
+    var access = document.getElementById("AboutMe");
+    access.scrollIntoView({behavior: 'smooth'}, true);
+}
+function scrolltoHome(){
+    var access = document.getElementById("Home");
+    access.scrollIntoView({behavior: 'smooth'}, true);
+}
+
+function Contact(){
+    var access = document.getElementById("Contact");
+    access.scrollIntoView({behavior: 'smooth'}, true);
+}
